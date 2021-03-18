fdb954c75fcad7319cb189cb410e3167e2f76802
Emily Hosoya <emilyhosoya@gmail.com>
2021-03-17 18:31:39 -0600

    trying this solution code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=maciek2021
+export POSTGRES_HOST=udagram.cfw0vo4twfkw.us-west-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-696988530455-dev
+export AWS_REGION=us-west-1
+export AWS_PROFILE=default
+export JWT_SECRET=meow
+export URL=http://localhost:8100
